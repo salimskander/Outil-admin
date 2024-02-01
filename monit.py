789fe1ff76adad2e9bc7c5970873de4fcfbbc917
ssalim <salim.skander@ynov.com>
2024-02-01 15:27:40 +0100

    monit.py

--- monit.py
+++ monit.py
@@ -7,11 +7,12 @@ import datetime
 import socket
 import logging
 from logging.handlers import RotatingFileHandler
+import requests
 
 # Configurations
-LOG_DIR = "/var/log/monit"
+LOG_DIR = "/var/monit"
 CONFIG_FILE_PATH = "/etc/monit/monit_config.json"
-DATA_DIR = "/var/monit/data"
+REPORTS_DIR = "/var/monit/reports"
 
 
 def setup_logging():
@@ -32,7 +33,7 @@ def load_config():
     if not os.path.exists(CONFIG_FILE_PATH):
         os.makedirs(os.path.dirname(CONFIG_FILE_PATH), exist_ok=True)
         with open(CONFIG_FILE_PATH, "w") as config_file:
-            json.dump({"tcp_ports": [], "alert_thresholds": {"cpu": 90, "ram": 20, "disk": 95}}, config_file, indent=2)
+            json.dump({"ports": [],"alert_thresholds": {"cpu": 90,"ram": 20,"disk": 95},"discord_webhook_url": "YOUR_DISCORD_WEBHOOK_URL"}, config_file, indent=2)
 
     with open(CONFIG_FILE_PATH, "r") as config_file:
         return json.load(config_file)
@@ -44,7 +45,7 @@ def check_resources():
     disk_percent = psutil.disk_usage("/").percent
 
     config = load_config()
-    tcp_ports_to_monitor = config.get("tcp_ports", [])
+    ports_to_monitor = config.get("ports", [])
     alert_thresholds = config.get("alert_thresholds", {})
 
     # Check CPU, RAM, Disk
@@ -54,9 +55,9 @@ def check_resources():
         if value > threshold:
             alert_values[resource] = value
 
-    # Check TCP Ports
+    # Check Ports
     ports_status = {}
-    for port in tcp_ports_to_monitor:
+    for port in ports_to_monitor:
         ports_status[port] = is_port_open("127.0.0.1", port)
 
     report = {
@@ -68,7 +69,7 @@ def check_resources():
         "ports_status": ports_status,
     }
 
-    report_file = os.path.join(DATA_DIR, f"report_{report['id']}.json")
+    report_file = os.path.join(REPORTS_DIR, f"report_{report['id']}.json")
     with open(report_file, "w") as report_file:
         json.dump(report, report_file, indent=2)
 
@@ -78,14 +79,25 @@ def check_resources():
     logging.info("Check completed and report generated.")
     return report
 
-
 def send_alert(alert_values):
-    # Adapt this function as needed for your specific alerting mechanism
-    logging.info("Sending alert: %s", alert_values)
+    config = load_config()
+    discord_webhook_url = config.get("discord_webhook_url")
+
+    if discord_webhook_url:
+        alert_message = "Alert! The following thresholds have been exceeded:\n"
+        for resource, value in alert_values.items():
+            alert_message += f"{resource}: {value}%\n"
+
+        payload = {"content": alert_message}
+        requests.post(discord_webhook_url, json=payload)
+        logging.info("Alert sent to Discord.")
+    else:
+        logging.warning("Discord webhook URL not configured. Unable to send alerts.")
+
 
 
 def list_reports():
-    reports = [f for f in os.listdir(DATA_DIR) if f.startswith("report_")]
+    reports = [f for f in os.listdir(REPORTS_DIR) if f.startswith("report_")]
     logging.info("List of available reports: %s", reports)
     return reports
 
@@ -94,7 +106,7 @@ def get_last_report():
     reports = list_reports()
     if reports:
         latest_report = max(reports)
-        report_file = os.path.join(DATA_DIR, latest_report)
+        report_file = os.path.join(REPORTS_DIR, latest_report)
         with open(report_file, "r") as report_file:
             last_report = json.load(report_file)
         logging.info("Retrieved the last report.")
@@ -114,7 +126,7 @@ def get_average_report(last_x_hours):
         average_report = {"cpu_percent": 0, "ram_percent": 0, "disk_percent": 0}
 
         for report_id in recent_reports:
-            report_file = os.path.join(DATA_DIR, report_id)
+            report_file = os.path.join(REPORTS_DIR, report_id)
             with open(report_file, "r") as report_file:
                 report_data = json.load(report_file)
 
@@ -133,9 +145,8 @@ def get_average_report(last_x_hours):
         logging.warning("No reports available in the specified time range.")
         return None
 
-
 def is_within_last_x_hours(report_id, last_x_hours):
-    report_file = os.path.join(DATA_DIR, report_id)
+    report_file = os.path.join(REPORTS_DIR, report_id)
     with open(report_file, "r") as report_file:
         report_data = json.load(report_file)
 
@@ -143,6 +154,7 @@ def is_within_last_x_hours(report_id, last_x_hours):
     time_difference = datetime.datetime.now() - report_time
 
     return time_difference.total_seconds() / 3600 <= last_x_hours
+
 
 
 def is_port_open(host, port):
@@ -156,42 +168,43 @@ def is_port_open(host, port):
 if __name__ == "__main__":
     setup_logging()
 
-    if not os.path.exists(DATA_DIR):
-        os.makedirs(DATA_DIR)
+    if not os.path.exists(REPORTS_DIR):
+        os.makedirs(REPORTS_DIR)
 
     # Commands
     if "check" in os.sys.argv:
         report = check_resources()
         print("Report generated:\n")
-        print(f"id : {report['id']}")
-        print(f"timestamp : {report['timestamp']}")
-        print(f"cpu_percent : {str(report['cpu_percent'])}%")
-        print(f"ram_percent : {str(report['ram_percent'])}%")
-        print(f"disk_percent : {str(report['disk_percent'])}%")
-        print(f"ports_status : {str(report['ports_status'])}")
+        print(f"id : {report["id"]}")
+        print(f"timestamp : {report["timestamp"]}")
+        print(f"cpu_percent : {str(report["cpu_percent"])}%")
+        print(f"ram_percent : {str(report["ram_percent"])}%")
+        print(f"disk_percent : {str(report["disk_percent"])}%")
+        print(f"ports_status : {str(report["ports_status"])}")
 
     elif "list" in os.sys.argv:
-        list_reports()
+        listReports = list_reports()
+        print("Lists of available reports:\n")
+        for report in listReports:
+            print(report)
 
     elif "get" in os.sys.argv and "last" in os.sys.argv:
-        last_report = get_last_report()
-        if last_report:
-            print("Last report: \n")
-            print(f"id : {last_report['id']}")
-            print(f"timestamp : {last_report['timestamp']}")
-            print(f"cpu_percent : {str(last_report['cpu_percent'])}%")
-            print(f"ram_percent : {str(last_report['ram_percent'])}%")
-            print(f"disk_percent : {str(last_report['disk_percent'])}%")
-            print(f"ports_status : {str(last_report['ports_status'])}")
+        report = get_last_report()
+        print("Last report: \n")
+        print(f"id : {report["id"]}")
+        print(f"timestamp : {report["timestamp"]}")
+        print(f"cpu_percent : {str(report["cpu_percent"])}%")
+        print(f"ram_percent : {str(report["ram_percent"])}%")
+        print(f"disk_percent : {str(report["disk_percent"])}%")
+        print(f"ports_status : {str(report["ports_status"])}")
 
     elif "get" in os.sys.argv and "avg" in os.sys.argv and len(os.sys.argv) == 4:
         last_x_hours = int(os.sys.argv[3])
-        average_report = get_average_report(last_x_hours)
-        if average_report:
-            print(f"Average report for the last {last_x_hours} hours: \n")
-            print(f"cpu_percent : {str(average_report['cpu_percent'])}%")
-            print(f"ram_percent : {str(average_report['ram_percent'])}%")
-            print(f"disk_percent : {str(average_report['disk_percent'])}%")
+        report = get_average_report(last_x_hours)
+        print(f"Average report for the last {last_x_hours} hours: \n")
+        print(f"cpu_percent : {str(report["cpu_percent"])}%")
+        print(f"ram_percent : {str(report["ram_percent"])}%")
+        print(f"disk_percent : {str(report["disk_percent"])}%")
 
     else:
         print("Usage: python monit.py [check | list | get last | get avg X]")
